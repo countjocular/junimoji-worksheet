a2b2c1803f7026d1eea0b363e486652012474d61
Count Jocular <i.am.count.jocular@gmail.com>
2020-11-11 23:42:13 +0000

    More robust URL generation

--- getQueryFromClues.js
+++ getQueryFromClues.js
@@ -4,6 +4,7 @@ function showPermalink() {
     if(i != 1) {cluesString = cluesString+'-';}
     cluesString = cluesString + document.getElementById("clue-"+i+"-input").value;
   }
-  var permalink = document.location.host+document.location.pathname+'/?clues='+cluesString;
+  var permalink = window.location.href.split('?')[0];
+  permalink = permalink+'?clues='+cluesString;
   document.getElementById("show-permalink").innerHTML = "If you want to link directly to this specific junimoji, use this link:<br /><a href=\""+permalink+"\">"+permalink+"</a><br /><small>Please note: this is beta functionality. Links may not work with later versions of this page.</small>"
 }
